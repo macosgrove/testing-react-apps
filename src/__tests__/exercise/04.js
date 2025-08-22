10fc12fdfa419051938d58b34e23a5ebbc02cdc9
macosgrove <maryanne@covidence.org>
2025-08-22 16:28:30 +1000

    Exercise 4.3

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -7,8 +7,12 @@ import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 import faker from 'faker'
 
-const buildLoginForm = () => {
-  return {username: faker.internet.userName(), password: faker.internet.password()}
+const buildLoginForm = (overrides) => {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    ...overrides
+  }
 }
 
 test('submitting the form calls onSubmit with username and password', async() => {
@@ -25,6 +29,23 @@ test('submitting the form calls onSubmit with username and password', async() =>
   expect(submit).toBeInTheDocument()
   await userEvent.click(submit)
   expect(handleSubmit).toHaveBeenCalledWith({username, password})
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+})
+
+test('password override', async() => {
+  const handleSubmit = jest.fn()
+  render(<Login onSubmit={handleSubmit} />)
+  const {username, password} = buildLoginForm({password: "thisPasswordFulfillsStrengthCriteria"})
+  const userNameField = screen.getByLabelText("Username")
+  expect(userNameField).toBeInTheDocument()
+  const passwordField = screen.getByLabelText("Password")
+  expect(passwordField).toBeInTheDocument()
+  await userEvent.type(userNameField, username)
+  await userEvent.type(passwordField, password)
+  const submit = screen.getByRole('button', {name: /submit/i})
+  expect(submit).toBeInTheDocument()
+  await userEvent.click(submit)
+  expect(handleSubmit).toHaveBeenCalledWith({username, password: "thisPasswordFulfillsStrengthCriteria"})
   expect(handleSubmit).toHaveBeenCalledTimes(1)
 })
 
