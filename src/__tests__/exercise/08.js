24ee20b206b3b96f178bcddf2f144889c4f8ee03
macosgrove <maryanne@covidence.org>
2025-10-30 17:36:28 +1100

    Exercise 8.1

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,39 +2,25 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render, screen, act} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
 
-// 🐨 create a simple function component that uses the useCounter hook
-// and then exposes some UI that our test can interact with to test the
-// capabilities of this hook
-// 💰 here's how to use the hook:
-// const {count, increment, decrement} = useCounter()
+let result
 function TestCounter() {
-  const {count, increment, decrement} = useCounter()
-  return (
-    <div>
-      <div id="label">Counter</div>
-      <div aria-labelledby="label">{count}</div>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-    </div>
-  )
+  result = useCounter()
+  return null
 }
 
-test('exposes the count and increment/decrement functions', async () => {
+test('exposes the count and increment/decrement functions', () => {
   render(<TestCounter />)
-  const count = screen.getByLabelText(/counter/i)
-  expect(count).toHaveTextContent('0')
-  const increment = screen.getByRole('button', {name: /increment/i})
-  await userEvent.click(increment)
-  expect(count).toHaveTextContent('1')
-  const decrement = screen.getByRole('button', {name: /decrement/i})
-  await userEvent.click(decrement)
-  expect(count).toHaveTextContent('0')
-  await userEvent.click(decrement)
-  expect(count).toHaveTextContent('-1')
+  expect(result.count).toEqual(0)
+  act(()=> result.increment())
+  expect(result.count).toEqual(1)
+  act(()=> result.decrement())
+  expect(result.count).toEqual(0)
+  act(()=> result.decrement())
+  expect(result.count).toEqual(-1)
 })
 
 /* eslint no-unused-vars:0 */
