135bbaa5b8e167dd2305b09cbf9dc57ae685faf8
macosgrove <maryanne@covidence.org>
2025-10-31 15:16:17 +1100

    Exercise 8.2

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -6,14 +6,18 @@ import {render, screen, act} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
 
-let result
-function TestCounter() {
-  result = useCounter()
+let result={}
+function TestComponent(props) {
+  Object.assign(result, useCounter(props))
   return null
 }
 
+function setup(props) {
+  render(<TestComponent {...props}/>)
+}
+
 test('exposes the count and increment/decrement functions', () => {
-  render(<TestCounter />)
+  setup()
   expect(result.count).toEqual(0)
   act(()=> result.increment())
   expect(result.count).toEqual(1)
@@ -21,6 +25,28 @@ test('exposes the count and increment/decrement functions', () => {
   expect(result.count).toEqual(0)
   act(()=> result.decrement())
   expect(result.count).toEqual(-1)
+})
+
+test('allows customization of the initial count', () => {
+  setup({ initialCount: 10})
+  expect(result.count).toEqual(10)
+  act(()=> result.increment())
+  expect(result.count).toEqual(11)
+  act(()=> result.decrement())
+  expect(result.count).toEqual(10)
+  act(()=> result.decrement())
+  expect(result.count).toEqual(9)
+})
+
+test('allows customization of the step', () => {
+  setup({ step: 2})
+  expect(result.count).toEqual(0)
+  act(()=> result.increment())
+  expect(result.count).toEqual(2)
+  act(()=> result.decrement())
+  expect(result.count).toEqual(0)
+  act(()=> result.decrement())
+  expect(result.count).toEqual(-2)
 })
 
 /* eslint no-unused-vars:0 */
